fe067563c25f55484df503fad335466c22c86c07
zzzzzzqwe <dima.gachayev@gmail.com>
2024-04-16 15:49:41 +0300

    minor changes

--- LaboratoryWork1/pangram.js
+++ LaboratoryWork1/pangram.js
@@ -20,6 +20,5 @@ function isPangram(sentence) {
     
 }
 // тесты с мудла
-
 console.log(isPangram('The quick brown fox jumps over the lazy dog.')); // true
 console.log(isPangram('This is not a pangram.')) // false
