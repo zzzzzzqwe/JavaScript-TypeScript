4585327681f4e42ae946119a552ed48811e9b101
zzzzzzqwe <dima.gachayev@gmail.com>
2024-04-20 00:07:42 +0300

    changes

---
+++ IndividualWork2/src/activity.js
@@ -0,0 +1,10 @@
+export async function getRandomActivity() {
+    try {
+        const response = await fetch('https://www.boredapi.com/api/activity/');
+        const data = await response.json();
+        return data.activity;
+    } catch (error) {
+        console.error('Произошла ошибка:', error);
+        return null;
+    }
+}
